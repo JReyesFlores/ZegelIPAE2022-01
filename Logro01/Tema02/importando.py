399d0f5db793dd4e1b559055320a8100182fb664
jhon.phileppe.reyes.flores <jhon.reyes@openserver.com.pe>
2022-04-18 21:05:35 -0500

    Agregando comentarios

--- Logro01/Tema02/importando.py
+++ Logro01/Tema02/importando.py
@@ -4,3 +4,9 @@ import operaciones as oper
 
 # Para acceder a las funciones debemos primero acceder a la importación del módulo
 oper.multiplicar(15, 4)
+
+# En esta sección solo importamos la función de sumar
+# Esto con la finalidad de poder usar dicha función sin hacer una referencia al módulo
+from operaciones import sumar
+
+sumar(10, 15)
