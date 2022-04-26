c459e2a634669d7676f122719773666d6ab832c5
jhon.phileppe.reyes.flores <jhon.reyes@openserver.com.pe>
2022-04-25 23:28:49 -0500

    Agregando comentarios

--- Logro01/Tema03/script.py
+++ Logro01/Tema03/script.py
@@ -1,5 +1,11 @@
+# importamos la función "saludar" desde el sub-paquete "hola" que a su 
+# vez se encuentra en el paquete llamado "paquete".
 from paquete.hola.saludos import saludar
 from paquete.adios.despedidas import Despedida
 
 saludar()
 Despedida()
+
+# cabe mencionar que es importante ubicar la ubicación de este archivo
+# "script.py" pues dependendiendo del nivel en el que se encuentre empezaremos a
+# llamar los paquetes y sub-paquetes.
